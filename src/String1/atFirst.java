8a2773b50b30551548cb6d8b4c7275e28a7a04d1
Taf22 <a.m.mansori21@gmail.com>
2025-07-31 01:43:11 +0400

    add sol for String1-atFirst

---
+++ src/String1/atFirst.java
@@ -0,0 +1,19 @@
+package String1;
+
+public class atFirst {
+    public static void main(String[] args) {
+        System.out.println(atFirst("hello"));
+        System.out.println(atFirst("hi"));
+        System.out.println(atFirst("h"));
+        System.out.println(atFirst(""));
+    }
+    public static String atFirst(String str) {
+        if (str.length() >= 2) {
+            return str.substring(0, 2);
+        } else if (str.length() == 1) {
+            return str + "@";
+        } else {
+            return "@@";
+        }
+    }
+}
