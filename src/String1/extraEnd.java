6225ce2b6357c1474a5860fee9a5f1f4e727999b
Taf22 <a.m.mansori21@gmail.com>
2025-07-30 22:38:01 +0400

    Add solution for string1-extraEnd

---
+++ src/String1/extraEnd.java
@@ -0,0 +1,19 @@
+package String1;
+
+public class extraEnd {
+
+        public static void main(String[] args) {
+            extraEnd obj = new extraEnd();
+            System.out.println(obj.extraEnd("Hello")); 
+            System.out.println(obj.extraEnd("ab"));
+            System.out.println(obj.extraEnd("Hi"));
+        }
+        public String extraEnd(String str) {
+            // Get the last two characters
+            String lastTwo = str.substring(str.length() - 2);
+            // Return three copies of lastTwo concatenated
+            return lastTwo + lastTwo + lastTwo;
+        }
+    }
+
+
